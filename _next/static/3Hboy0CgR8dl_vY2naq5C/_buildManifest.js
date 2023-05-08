59072dccc79ed0659f819279551bd52c9a4a0e7a
1F10230183 <1F10230183@users.noreply.github.com>
2023-05-08 18:21:49 +0000

    deploy: 57ee03ae77748864d104e49b448425e6d360077c

---
+++ _next/static/3Hboy0CgR8dl_vY2naq5C/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/2a6ad69eabab3a5a.css","static/chunks/pages/index-a38c4c91494f193f.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
