588c897321a06664b15563bdf8f2bd54d2d87dfb
1F10230183 <1F10230183@users.noreply.github.com>
2023-05-09 14:55:10 +0000

    deploy: 39827187389fb3275a2d5226c66b1777ddbdaf79

---
+++ _next/static/aPyeU0NZ6BjlXI1jh2YZZ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/2a6ad69eabab3a5a.css","static/chunks/pages/index-416a928b6b1d9909.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
