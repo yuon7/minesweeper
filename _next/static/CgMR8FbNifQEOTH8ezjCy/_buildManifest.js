cb1dbbb788a5c43217d67add7becefcaaeb29d87
yuon7 <yuon7@users.noreply.github.com>
2024-06-24 12:03:02 +0000

    deploy: 406ff43ef7eb0039f8f19d744a297dad90b383ff

--- _next/static/CgMR8FbNifQEOTH8ezjCy/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/32988da42380a6db.css","static/chunks/pages/index-16a5100afd28340f.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
